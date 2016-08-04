8746cc011d63be67d9be18c5ecf5058bdf441f01
Guillaume Malette <guillaume@shopify.com>
2016-08-04 06:17:09 -0400

    Metric channels routes

--- web/channels/user_socket.ex
+++ web/channels/user_socket.ex
@@ -2,11 +2,11 @@ defmodule Dasher.UserSocket do
   use Phoenix.Socket
 
   ## Channels
-  # channel "room:*", Dasher.RoomChannel
+  channel "metric:lobby", Dasher.MetricChannel
 
   ## Transports
   transport :websocket, Phoenix.Transports.WebSocket
-  # transport :longpoll, Phoenix.Transports.LongPoll
+  transport :longpoll, Phoenix.Transports.LongPoll
 
   # Socket params are passed from the client and can
   # be used to verify and authenticate a user. After
--- web/router.ex
+++ web/router.ex
@@ -18,9 +18,4 @@ defmodule Dasher.Router do
 
     get "/", PageController, :index
   end
-
-  # Other scopes may use custom stacks.
-  # scope "/api", Dasher do
-  #   pipe_through :api
-  # end
 end
