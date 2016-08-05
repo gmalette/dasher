df0ea2d284ca2242340bd524b1bfec2af98e1039
Guillaume Malette <guillaume@shopify.com>
2016-08-04 20:16:07 -0400

    Use the alias on Dasher.Metrics

--- test/models/metrics_test.exs
+++ test/models/metrics_test.exs
@@ -14,19 +14,19 @@ defmodule Dasher.MetricTest do
 
   setup do
     {:ok, events} = GenEvent.start_link
-    {:ok, pid} = Dasher.Metrics.start_link(events)
+    {:ok, pid} = Metrics.start_link(events)
     GenEvent.add_mon_handler(events, Forwarder, self())
 
     {:ok, pid: pid, events: events}
   end
 
   test "reading unknown metric returns an error", %{pid: pid} do
-    assert :error == Dasher.Metrics.get(pid, "toto")
+    assert :error == Metrics.get(pid, "toto")
   end
 
   test "can add values", %{pid: pid} do
-    assert :ok == Dasher.Metrics.add(pid, "toto", :value, 42)
-    assert {:ok, {:value, 42}} == Dasher.Metrics.get(pid, "toto")
+    assert :ok == Metrics.add(pid, "toto", :value, 42)
+    assert {:ok, {:value, 42}} == Metrics.get(pid, "toto")
   end
 
   test "can add histograms", %{pid: pid} do
@@ -36,34 +36,34 @@ defmodule Dasher.MetricTest do
       %{x: 2, y: 5},
       %{x: 3, y: 3},
     ]
-    assert :ok == Dasher.Metrics.add(pid, "toto", :histogram, values)
-    assert {:ok, {:histogram, values}} == Dasher.Metrics.get(pid, "toto")
+    assert :ok == Metrics.add(pid, "toto", :histogram, values)
+    assert {:ok, {:histogram, values}} == Metrics.get(pid, "toto")
   end
 
   test "can't add invalid histograms", %{pid: pid} do
     values = [
       %{a: 0, y: 10},
     ]
-    assert :error == Dasher.Metrics.add(pid, "toto", :histogram, values)
+    assert :error == Metrics.add(pid, "toto", :histogram, values)
 
     values = [
       %{x: 0, y: "a"},
     ]
-    assert :error == Dasher.Metrics.add(pid, "toto", :histogram, values)
+    assert :error == Metrics.add(pid, "toto", :histogram, values)
   end
 
   test "can add arbitrary data", %{pid: pid} do
-    assert :ok == Dasher.Metrics.add(pid, "toto", :arbitrary, 42)
-    assert :ok == Dasher.Metrics.add(pid, "toto", :arbitrary, [42])
-    assert :ok == Dasher.Metrics.add(pid, "toto", :arbitrary, %{a: 42})
+    assert :ok == Metrics.add(pid, "toto", :arbitrary, 42)
+    assert :ok == Metrics.add(pid, "toto", :arbitrary, [42])
+    assert :ok == Metrics.add(pid, "toto", :arbitrary, %{a: 42})
   end
 
   test "can't add unknown values", %{pid: pid} do
-    assert :error == Dasher.Metrics.add(pid, "toto", :blog, "my personal blog")
+    assert :error == Metrics.add(pid, "toto", :blog, "my personal blog")
   end
 
   test "adding a metric sends a :refresh event", %{pid: pid} do
-    assert :ok == Dasher.Metrics.add(pid, "toto", :value, 42)
+    assert :ok == Metrics.add(pid, "toto", :value, 42)
     assert_received {:refresh, "toto"}
   end
 end
